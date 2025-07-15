464f1ee6be8a11e74adaa8960c0ad3748a7c0d73
MDShadab-DEV <shahabkhan65777@gmail.com>
2025-07-15 22:46:10 +0530

    tags added in Test Runner File

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,10 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"})
+@CucumberOptions(features="src/test/java/features",plugin ={"json:target/jsonReports/cucumber-report.json","html:target/cucumber.html"},tags="@DeletePlace",glue= "stepDefinations")
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
 }
+
+
+//mvn test -Dcucumber.filter.tags="@DeletePlace" -  Example Command to Run Tests by Tag
